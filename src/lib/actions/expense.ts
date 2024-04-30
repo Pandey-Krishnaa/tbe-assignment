f9dfd60652f10687a3c52a637d499285671a3866
krish36pandey@gmail.com <krish36pandey@gmail.com>
2024-04-30 20:09:14 +0530

    added check to ensure the category model is loaded or not

--- src/lib/actions/expense.ts
+++ src/lib/actions/expense.ts
@@ -1,8 +1,9 @@
 "use server";
-import category from "../models/category";
+import category, { schema, schema } from "../models/category";
 import expense from "./../models/expense";
 
 import { connectToDatabase } from "../db/mongoose";
+import mongoose from "mongoose";
 
 export const addExpense = async (data: any) => {
   try {
@@ -33,6 +34,10 @@ export const deleteExpense = async (id: string) => {
 export const getAllExpenses = async () => {
   try {
     await connectToDatabase();
+    if (!mongoose.models.category) {
+      // If Category model is not registered, import and register it
+      mongoose.model("category", schema);
+    }
     const expenses = await expense.find({}).populate("category");
     return expenses;
   } catch (error) {
--- src/lib/models/category.ts
+++ src/lib/models/category.ts
@@ -1,5 +1,5 @@
 import { Schema, model, models } from "mongoose";
-const schema = new Schema({
+export const schema = new Schema({
   category: {
     type: String,
     required: [true, "category is required"],
